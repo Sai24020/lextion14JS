08424aec4e8a5635a36d711f9fc219e3a88d7235
Suzan Al Majdalawi <suzanmajdalawi1@gmail.com>
2025-01-14 11:00:59 +0100

    JS object och JSON

---
+++ script.js
@@ -0,0 +1,38 @@
+const fruit = {
+    name: "Apple",
+    isNutririous: true,
+    color: "Green"
+
+}
+
+
+//JSON
+
+const myJson = {
+    "fruits": [
+        {
+        "id": 1,
+        "name":"Apple", 
+        "isNutririous": true,
+        "color": "green"
+
+    },
+    {
+        "id": 2,
+        "name":"Pear", 
+        "isNutririous": true,
+        "color": "green"
+    },
+    {
+        "id": 3,
+        "name":"Banana", 
+        "isNutririous": true,
+        "color": "green"
+    }
+  ]
+}
+
+console.log(myJson.fruits);
+const stringify = JSON.stringify(myJson);
+console.log(stringify);
+console.log(window);
